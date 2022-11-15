fa8e773c1cdd4bd79023a4bef38285f1613406d3
zdni <alzidni.kasim@gmail.com>
2022-11-16 00:30:13 +0800

    revisi

--- database/jelajah_sultra.sql
+++ database/jelajah_sultra.sql
@@ -1,178 +1,138 @@
--- phpMyAdmin SQL Dump
--- version 5.1.1deb5ubuntu1
--- https://www.phpmyadmin.net/
+-- MySQL dump 10.13  Distrib 8.0.31, for Linux (x86_64)
 --
--- Host: localhost:3306
--- Waktu pembuatan: 07 Okt 2022 pada 21.19
--- Versi server: 8.0.30-0ubuntu0.22.04.1
--- Versi PHP: 8.1.2
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-START TRANSACTION;
-SET time_zone = "+00:00";
-
+-- Host: localhost    Database: jelajah_sultra
+-- ------------------------------------------------------
+-- Server version	8.0.31-0ubuntu0.20.04.1
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
--- Database: `jelajah_sultra`
+-- Table structure for table `kategori`
 --
 
--- --------------------------------------------------------
-
---
--- Struktur dari tabel `kategori`
---
-
+DROP TABLE IF EXISTS `kategori`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `kategori` (
-  `id` int NOT NULL,
-  `nama` varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+  `id` int NOT NULL AUTO_INCREMENT,
+  `nama` varchar(255) NOT NULL,
+  `deskripsi` text NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data untuk tabel `kategori`
+-- Dumping data for table `kategori`
 --
 
-INSERT INTO `kategori` (`id`, `nama`) VALUES
-(2, 'Pantai'),
-(3, 'Gunung');
-
--- --------------------------------------------------------
+LOCK TABLES `kategori` WRITE;
+/*!40000 ALTER TABLE `kategori` DISABLE KEYS */;
+INSERT INTO `kategori` VALUES (2,'Pantai',''),(3,'Gunung',''),(4,'Taman Nasional',''),(5,'Goa',''),(6,'Danau',''),(7,'Air Terjun',''),(8,'Budaya',''),(9,'Sejarah',''),(10,'Religi',''),(11,'Tanjung',''),(12,'Pulau dan Kepulauan','');
+/*!40000 ALTER TABLE `kategori` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
--- Struktur dari tabel `roles`
+-- Table structure for table `roles`
 --
 
+DROP TABLE IF EXISTS `roles`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `roles` (
-  `id` int NOT NULL,
-  `name` varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+  `id` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data untuk tabel `roles`
+-- Dumping data for table `roles`
 --
 
-INSERT INTO `roles` (`id`, `name`) VALUES
-(1, 'Admin');
-
--- --------------------------------------------------------
+LOCK TABLES `roles` WRITE;
+/*!40000 ALTER TABLE `roles` DISABLE KEYS */;
+INSERT INTO `roles` VALUES (1,'Admin');
+/*!40000 ALTER TABLE `roles` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
--- Struktur dari tabel `users`
+-- Table structure for table `users`
 --
 
+DROP TABLE IF EXISTS `users`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `users` (
-  `id` int NOT NULL,
+  `id` int NOT NULL AUTO_INCREMENT,
   `username` varchar(255) NOT NULL,
   `name` varchar(255) NOT NULL,
   `email` varchar(255) NOT NULL,
   `password` text NOT NULL,
   `image` text NOT NULL,
-  `role_id` int NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+  `role_id` int NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `role_id` (`role_id`),
+  CONSTRAINT `users_ibfk_1` FOREIGN KEY (`role_id`) REFERENCES `roles` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data untuk tabel `users`
+-- Dumping data for table `users`
 --
 
-INSERT INTO `users` (`id`, `username`, `name`, `email`, `password`, `image`, `role_id`) VALUES
-(1, 'admin', 'Administrator', 'admin@gmail.com', '$2y$10$uGSBRKsWu2Xv0.xCmtNxPe52W2f10pLAqdlyK4o4WVbshywCmvaOe', 'admin.png', 1);
-
--- --------------------------------------------------------
+LOCK TABLES `users` WRITE;
+/*!40000 ALTER TABLE `users` DISABLE KEYS */;
+INSERT INTO `users` VALUES (1,'admin','Administrator','admin@gmail.com','$2y$10$uGSBRKsWu2Xv0.xCmtNxPe52W2f10pLAqdlyK4o4WVbshywCmvaOe','admin.png',1);
+/*!40000 ALTER TABLE `users` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
--- Struktur dari tabel `wisata`
+-- Table structure for table `wisata`
 --
 
+DROP TABLE IF EXISTS `wisata`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `wisata` (
-  `id` int NOT NULL,
+  `id` int NOT NULL AUTO_INCREMENT,
   `kategori_id` int NOT NULL,
   `nama` text NOT NULL,
   `jam_operasional` varchar(255) NOT NULL,
   `fasilitas` text NOT NULL,
   `lokasi` text NOT NULL,
   `keterangan` text NOT NULL,
-  `image` varchar(255) NOT NULL DEFAULT 'wisata.jpg'
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+  `image` varchar(255) NOT NULL DEFAULT 'wisata.jpg',
+  `map` text NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data untuk tabel `wisata`
+-- Dumping data for table `wisata`
 --
 
-INSERT INTO `wisata` (`id`, `kategori_id`, `nama`, `jam_operasional`, `fasilitas`, `lokasi`, `keterangan`, `image`) VALUES
-(1, 2, 'Pantai Toronipa', '24 Jam', 'Pondok - Pondok, Warung, Ban, Wc, Aula', 'Desa Toronipa, Kecamatan Soropia, Kabupaten Konawe, Sulawesi Tenggara 93351, Indonesia.', 'Pantai Toronipa berasal dari bahasa Bugis, yaitu toro nipa yang berarti pohon nipa yang turun. Beberapa pohon nipa memang masih tampak terlihat di dekat gerbang masuk pantai. Panorama alam, deburan ombak Laut Banda serta pasir putih yang terbentang luas merupakan perpaduan yang asyik untuk dinikmati. Juga bibir pantai yang dipenuhi pohon kelapa membuat suasana pantai semakin asri, sejuk dan damai. Selain itu Pantai Toronipa juga sangat landai, bahkan hingga 1 km ke arah laut tinggi air masih hanya sedalam 1 meter saja.', 'wisata1665140770.jpg');
+LOCK TABLES `wisata` WRITE;
+/*!40000 ALTER TABLE `wisata` DISABLE KEYS */;
+INSERT INTO `wisata` VALUES (1,2,'Pantai Toronipa','24 Jam','Pondok - Pondok, Warung, Ban, Wc, Aula','Desa Toronipa, Kecamatan Soropia, Kabupaten Konawe, Sulawesi Tenggara 93351, Indonesia.','Pantai Toronipa berasal dari bahasa Bugis, yaitu toro nipa yang berarti pohon nipa yang turun. Beberapa pohon nipa memang masih tampak terlihat di dekat gerbang masuk pantai. Panorama alam, deburan ombak Laut Banda serta pasir putih yang terbentang luas merupakan perpaduan yang asyik untuk dinikmati. Juga bibir pantai yang dipenuhi pohon kelapa membuat suasana pantai semakin asri, sejuk dan damai. Selain itu Pantai Toronipa juga sangat landai, bahkan hingga 1 km ke arah laut tinggi air masih hanya sedalam 1 meter saja.','wisata1665140770.jpg','https://www.google.com/maps/place/Pantai+Toronipa/@-3.8973772,122.6563006,15z/data=!3m1!4b1!4m5!3m4!1s0x2d98e973940bd0d7:0x8d6717741f95e82e!8m2!3d-3.8973773!4d122.6650554');
+/*!40000 ALTER TABLE `wisata` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
---
--- Indexes for dumped tables
---
-
---
--- Indeks untuk tabel `kategori`
---
-ALTER TABLE `kategori`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indeks untuk tabel `roles`
---
-ALTER TABLE `roles`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indeks untuk tabel `users`
---
-ALTER TABLE `users`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `role_id` (`role_id`);
-
---
--- Indeks untuk tabel `wisata`
---
-ALTER TABLE `wisata`
-  ADD PRIMARY KEY (`id`);
-
---
--- AUTO_INCREMENT untuk tabel yang dibuang
---
-
---
--- AUTO_INCREMENT untuk tabel `kategori`
---
-ALTER TABLE `kategori`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
-
---
--- AUTO_INCREMENT untuk tabel `roles`
---
-ALTER TABLE `roles`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
-
---
--- AUTO_INCREMENT untuk tabel `users`
---
-ALTER TABLE `users`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
-
---
--- AUTO_INCREMENT untuk tabel `wisata`
---
-ALTER TABLE `wisata`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
-
---
--- Ketidakleluasaan untuk tabel pelimpahan (Dumped Tables)
---
-
---
--- Ketidakleluasaan untuk tabel `users`
---
-ALTER TABLE `users`
-  ADD CONSTRAINT `users_ibfk_1` FOREIGN KEY (`role_id`) REFERENCES `roles` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT;
-COMMIT;
-
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2022-11-08 17:39:25
